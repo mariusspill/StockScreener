4cfb38c6ad83aa8315e8ed813263a94f359befc1
Marius Spill <marius.spill@gmail.com>
2025-07-22 21:45:09 +0200

    small commit

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ import caching.caching as cch
 
 tckrs = tickers.getTickers("./helpers/list.txt")
 
-# ascreen.print_analysis(tckrs)
-
 def daily_fetch():
     ava.fetch_data()
     sql.fetch_sp500_from_alpha_advantage()
 
-# daily_fetch()
 
 def daily_cache():
     cch.cache_market_caps(tckrs)
