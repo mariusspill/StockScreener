1c928a0f118eca6940942e29dda5226149e0fc34
Marius Spill <marius.spill@gmail.com>
2025-07-26 13:58:20 +0200

    added v1 of web app dashboard

--- webinterface/static/script.js
+++ webinterface/static/script.js
@@ -15,21 +15,4 @@ async function sendParam() {
     } else {
         document.getElementById('result').innerText = 'Error fetching screening results';
     }
-    }            async function sendParam() {
-    const pe = document.getElementById('pe').value;
-
-    const response = await fetch('/screen', {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ pe: pe })
-    });
-
-    if (response.ok) {
-        const data = await response.json();
-        document.getElementById('result').innerText = JSON.stringify(data, null, 2);
-    } else {
-        document.getElementById('result').innerText = 'Error fetching screening results';
-    }
 }
